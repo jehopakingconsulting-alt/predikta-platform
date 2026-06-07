2c03418d206c7d3ad2cd643a16a2641f785d3421
JeHoPa KING <jehopakingconsulting@gmail.com>
2026-06-07 19:20:17 -0400

    Fix Connecticut: real draw labels are Day/Night (not Midday/Evening) - rebuilt clean balanced dataset (99/98) with correct labels and confirmed official times (1:57 PM / 10:29 PM ET)

--- app.py
+++ app.py
@@ -111,7 +111,7 @@ DRAW_SCHEDULE = {
     "MI": [{"tod":"Midday","time":"12:59 PM ET"}, {"tod":"Evening","time":"7:29 PM ET"}],
     "TN": [{"tod":"Midday","time":"1:00 PM CT"},  {"tod":"Evening","time":"6:29 PM CT"}],
     "SC": [{"tod":"Midday","time":"12:59 PM ET"}, {"tod":"Evening","time":"6:59 PM ET"}],
-    "CT": [{"tod":"Midday","time":"1:57 PM ET"},  {"tod":"Evening","time":"10:29 PM ET"}],
+    "CT": [{"tod":"Day","time":"1:57 PM ET"},  {"tod":"Night","time":"10:29 PM ET"}],
     "MD": [{"tod":"Midday","time":"12:28 PM ET"}, {"tod":"Evening","time":"7:56 PM ET"}],
     "MA": [{"tod":"Midday","time":"1:00 PM ET"},  {"tod":"Evening","time":"7:29 PM ET"}],
     "IN": [{"tod":"Midday","time":"1:20 PM ET"},  {"tod":"Evening","time":"8:00 PM ET"}],
