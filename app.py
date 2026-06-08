994c492bf161e2cf7c2fd0e589d4200e76409ba4
JeHoPa KING <jehopakingconsulting@gmail.com>
2026-06-07 20:14:59 -0400

    Fix VA: correct TOD labels Day/Night (was mislabeled Midday/Evening)

--- app.py
+++ app.py
@@ -107,7 +107,7 @@ DRAW_SCHEDULE = {
     "NJ": [{"tod":"Midday","time":"12:59 PM ET"}, {"tod":"Evening","time":"10:57 PM ET"}],
     "IL": [{"tod":"Midday","time":"12:40 PM CT"}, {"tod":"Evening","time":"9:20 PM CT"}],
     "NC": [{"tod":"Day","time":"3:00 PM ET"},  {"tod":"Evening","time":"11:22 PM ET"}],
-    "VA": [{"tod":"Midday","time":"1:59 PM ET"},  {"tod":"Evening","time":"11:00 PM ET"}],
+    "VA": [{"tod":"Day","time":"1:59 PM ET"},  {"tod":"Night","time":"11:00 PM ET"}],
     "MI": [{"tod":"Midday","time":"12:59 PM ET"}, {"tod":"Evening","time":"7:29 PM ET"}],
     "TN": [{"tod":"Morning","time":"9:28 AM CT"}, {"tod":"Midday","time":"12:28 PM CT"}, {"tod":"Evening","time":"6:28 PM CT"}],
     "SC": [{"tod":"Midday","time":"12:59 PM ET"}, {"tod":"Evening","time":"6:59 PM ET"}],
