88365d7557c7637fde4802c8e1bcf4afbc749309
JeHoPa KING <jehopakingconsulting@gmail.com>
2026-06-07 20:05:41 -0400

    Fix Rhode Island: correct draw times to 1:30 PM / 6:59 PM ET (were 1:00 PM / 7:29 PM)

--- app.py
+++ app.py
@@ -126,7 +126,7 @@ DRAW_SCHEDULE = {
     "KS": [{"tod":"Midday","time":"1:10 PM CT"}, {"tod":"Evening","time":"9:10 PM CT"}],
     "OK": [{"tod":"Evening","time":"9:10 PM CT"}],
     "DE": [{"tod":"Day","time":"1:58 PM ET"},  {"tod":"Night","time":"7:57 PM ET"}],
-    "RI": [{"tod":"Midday","time":"1:00 PM ET"},  {"tod":"Evening","time":"7:29 PM ET"}],
+    "RI": [{"tod":"Midday","time":"1:30 PM ET"},  {"tod":"Evening","time":"6:59 PM ET"}],
     "NM": [{"tod":"Day","time":"1:00 PM MT"}, {"tod":"Evening","time":"9:30 PM MT"}],
     "WV": [{"tod":"Midday","time":"12:53 PM ET"}, {"tod":"Evening","time":"7:29 PM ET"}],
     "DC": [{"tod":"Midday","time":"1:50 PM ET"},  {"tod":"Evening","time":"7:50 PM ET"}],
